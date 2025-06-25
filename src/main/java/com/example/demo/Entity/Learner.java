f05fac77608a795a926818eeb774ca506ed6e82a
Mounika <mounikaullenga0@gmail.com>
2025-06-25 12:27:22 +0530

    Initial commit for SkillSwap project

---
+++ src/main/java/com/example/demo/Entity/Learner.java
@@ -0,0 +1,49 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Learner {
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	private String username;
+	private String email;
+	private String password;
+	private String role;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+
+}
